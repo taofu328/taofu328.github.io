570cf4f3af4a2c3954ec713f5ad2f88670eae259
taofu <taofu328@gmail.com>
2020-02-22 01:07:43 +0800

    2020.02.22

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?432ac14d870534a923402b69cc1f6c9c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
